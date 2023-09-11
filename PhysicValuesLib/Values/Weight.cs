1d762c2974b889d84d7bce5f8bc9db603aeb46ae
1Chuppi1 <133848692+1Chuppi1@users.noreply.github.com>
2023-09-11 14:56:45 +0300

    Добавьте файлы проекта.

---
+++ PhysicValuesLib/Values/Weight.cs
@@ -0,0 +1,17 @@
+﻿namespace PhysicValuesLib.Values;
+
+internal class Weight : AnyValue
+{
+    public Weight()
+    {
+        ValueName = "Вес";
+
+        CoefficientsAndMeasuresList = new Dictionary<string, double>()
+        {
+           { "Грамм",      0.001},
+            { "Килограммы", 1},
+            { "Центнеры",   100},
+            { "Тонны",      1*1000},
+        };
+    }
+}
